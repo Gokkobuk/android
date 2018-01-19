eb8ab5476b116e09ff83be17b4bc47eb61fd5738
kaykwon <rud7736@gmail.com>
2018-01-19 13:50:19 +0900

    1.2018.01.19. android _ Retrofit2 호출 예제 with 디우 active

---
+++ Contributor.java
@@ -0,0 +1,15 @@
+package com.example.kaykwon.test1;
+
+/**
+ * Created by kaykwon on 2018-01-18.
+ */
+
+public class Contributor {
+    // login 정보를 받아올 것이므로
+    public final String login;
+
+    public Contributor(String login) {
+        this.login = login;
+    }
+
+}
